5e4a79fd9a9596e847f7a1e43a02120589ef48db
Alif Bhadrika Parikesit <alif.parikesit@finsoft.id>
2023-07-17 21:52:38 +0700

    change backend URI

--- src/configs/index.js
+++ src/configs/index.js
@@ -4,7 +4,8 @@ const config = {
     defaultClientPath: '/interview',
     fontFamily: `'Inter', sans-serif`,
     borderRadius: 8,
-    apiUrl: process.env.REACT_APP_BACKEND_URI || 'http://localhost:9000/v1'
+    // apiUrl: process.env.REACT_APP_BACKEND_URI || 'http://localhost:9000/v1',
+    apiUrl: process.env.REACT_APP_BACKEND_URI || 'https://backend-woeckfq5ga-as.a.run.app/v1'
 };
 
 export default config;
