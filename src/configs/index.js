3166b3acc0eedfab12e3216ec169259aa33256a6
Ryo Richardo <37143513+ryorichardo@users.noreply.github.com>
2024-04-01 22:46:38 +0700

    edit backend url

--- src/configs/index.js
+++ src/configs/index.js
@@ -5,7 +5,7 @@ const config = {
     fontFamily: `'Inter', sans-serif`,
     borderRadius: 8,
     // apiUrl: process.env.REACT_APP_BACKEND_URI || 'http://localhost:9000/v1',
-    apiUrl: process.env.REACT_APP_BACKEND_URI || 'https://backend-woeckfq5ga-as.a.run.app/v1'
+    apiUrl: process.env.REACT_APP_BACKEND_URI || 'https://backend-n4jmdbfb5a-uc.a.run.app/v1'
 };
 
 export default config;
